1c08de32fa06054ea5fd4b6f0b58ddd6a0817720
risv1 <rishi.aangarai@gmail.com>
2024-08-07 19:40:21 +0530

    feat: add custom mapreduce job

---
+++ mapreduce/WordCountMapper.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] words = value.toString().split("\\s+");
+        for (String str : words) {
+            word.set(str);
+            context.write(word, one);
+        }
+    }
+}
